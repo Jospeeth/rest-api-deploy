18ccf2c1f6870d8ba03dca4bd9d9c5f92c84a1fe
jose Pertuz <japertuzd@gmail.com>
2024-04-27 18:59:00 -0500

    added cros

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { corsMiddleware } from "./middlewares/cors.js";
 const app = express();
 app.disable("x-powered-by");
 app.use(json());
-
+app.use(corsMiddleware());
 
 app.use("/foods", foodsRouter);
 
